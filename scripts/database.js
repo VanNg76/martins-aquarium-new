136af05eea65b07a424a66d38531bc4aa136aa93
Van Nguyen <thanh_xer@yahoo.com>
2022-01-11 16:41:33 -0600

    1st database

--- scripts/database.js
+++ scripts/database.js
@@ -1,13 +1,35 @@
 const database = {
     fish: [
         {
-            image: "#",
-            spicies: "gold fish",
-            length: "10cm",
+            image: "images/gold-fish.jpg",
+            species: "gold fish",
+            length: "5 inches",
             name: "GOLD",
-            where-it-havested: "Atlantic Ocean",
+            where_it_havested: "Atlantic Ocean",
+            food: "smaller fish"
+        },
+        {
+            image: "images/dragon-fish.jpg",
+            species: "dragon fish",
+            length: "10 inches",
+            name: "DRAGON",
+            where_it_havested: "Pacific Ocean",
+            food: "smaller fish"
+        },
+        {
+            image: "images/orange-fish.jpg",
+            species: "orange fish",
+            length: "7 inches",
+            name: "ORANGE",
+            where_it_havested: "Medit Ocean",
             food: "smaller fish"
         }
     ]
 }
+
+export const getFish = () => {
+    return database.fish.map(fish => ({...fish}))
+}
+
+
 
