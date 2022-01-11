75f1a45639841da43e659846645c2209585c39d0
Van Nguyen <thanh_xer@yahoo.com>
2022-01-11 12:00:47 -0600

    final

---
+++ scripts/database.js
@@ -0,0 +1,13 @@
+const database = {
+    fish: [
+        {
+            image: "#",
+            spicies: "gold fish",
+            length: "10cm",
+            name: "GOLD",
+            where-it-havested: "Atlantic Ocean",
+            food: "smaller fish"
+        }
+    ]
+}
+
